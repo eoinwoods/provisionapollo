4a5fabebbc6978e5a5c062cb1a8aad3531bdad1c
Eoin Woods <github@copse.org.uk>
2018-05-19 22:26:19 +0000

    Added sysstat package to setup

--- bin/apollo-docker-setup.sh
+++ bin/apollo-docker-setup.sh
@@ -22,6 +22,7 @@ sudo add-apt-repository \
 sudo apt-get update -y
 sudo apt-get install -y docker-ce
 sudo docker run hello-world
+sudo apt-get install -y sysstat
 
 sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
