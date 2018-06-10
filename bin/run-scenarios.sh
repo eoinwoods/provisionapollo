bf3b2ebe7d80c47d865d63145e4df5b3faca3f18
Eoin Woods <github@copse.org.uk>
2018-06-10 17:18:47 +0000

    Refined script to run scenarios

--- bin/run-scenarios.sh
+++ bin/run-scenarios.sh
@@ -7,24 +7,29 @@
 ROOTDIR=${ROOTDIR:-$PWD}
 PATH=$PATH:$ROOTDIR/bin
 
+if [ $# -eq 0 ]
+then
+  scenario_list="single_cpu three_cpu single_cpu data-500mb-4times cpu-data-mix"
+else
+  scenario_list=$*
+fi
+
 clear-databases.sh
 
-echo "========== Waiting 30 seconds"
+echo "========== Waiting 30 seconds to collect initial data"
 sleep 30
 
-echo "========== Running scenarios"
-curl http://localhost:9999/invoke/single-cpu
-sleep 10
-curl http://localhost:9999/invoke/three-cpu
-sleep 10
-curl http://localhost:9999/invoke/single-cpu
-sleep 10
-curl http://localhost:9999/invoke/data-500mb-4times
-sleep 10
-curl http://localhost:9999/invoke/cpu-data-mix
+echo "========== Running scenarios: $scenario_list"
+for s in $scenario_list
+do
+   echo Running $s
+   curl http://localhost:9999/invoke/$s
+   echo Sleeping 5 seconds
+   sleep 5
+done
 
-echo "========== Waiting 30 seconds"
-sleep 30
+echo "========== Waiting another 25 seconds to collect data"
+sleep 20
 
 export-data.sh
 
