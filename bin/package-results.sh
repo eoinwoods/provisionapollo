a20ccd92a00b08ded114ebbbca942f7bcb97bcc8
Eoin Woods <github@copse.org.uk>
2018-06-30 20:38:55 +0000

    Added sar(1m) stats to scenario execution

--- bin/package-results.sh
+++ bin/package-results.sh
@@ -11,7 +11,8 @@ fi
 date_string=$(date +%Y%m%d-%H%M%S)
 file_name="${1}-${date_string}.tar.gz"
 
-file_list="zipkin_db.sql telegraf.line.dmp docker_network.json pidstat.out"
+file_list="zipkin_db.sql telegraf.line.dmp \
+           docker_network.json pidstat.out sar_stats.out"
 
 tar czf ${file_name} ${file_list}
 
--- bin/run-scenarios.sh
+++ bin/run-scenarios.sh
@@ -33,8 +33,18 @@ done
 echo "========== Waiting another 25 seconds to collect data"
 sleep 20
 
+# Stop writing process level statistics
 killall -HUP pidstat
 
+# Collect sar(1m) stats for CPU, I/O and memory from 10 mins ago to now
+# The explicit LC_TIME setting is to set 24 hour times
+let start_time_secs=$(date +%s)-600
+start_time="$(date -d @$start_time_secs +%H:%M:%S)"
+sar_output_file=sar_stats.out
+LC_TIME=C sar -P ALL -s $start_time > $sar_output_file   # CPU
+LC_TIME=C sar -r     -s $start_time >> $sar_output_file  # Memory
+LC_TIME=C sar -b     -s $start_time >> $sar_output_file  # I/O
+ 
 export-data.sh
 
 
