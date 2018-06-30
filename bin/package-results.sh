bf1261b3864a697e34c62ca40db677436d1906a5
Eoin Woods <github@copse.org.uk>
2018-06-30 22:22:14 +0000

    Remove compression from results packaging

--- bin/package-results.sh
+++ bin/package-results.sh
@@ -9,10 +9,10 @@ then
 fi
 
 date_string=$(date +%Y%m%d-%H%M%S)
-file_name="${1}-${date_string}.tar.gz"
+file_name="${1}-${date_string}.tar"
 
 file_list="zipkin_db.sql telegraf.line.dmp \
            docker_network.json pidstat.out sar_stats.out"
 
-tar czf ${file_name} ${file_list}
+tar cf ${file_name} ${file_list}
 
