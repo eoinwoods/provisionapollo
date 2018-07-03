f7e783b197d652c3906cfc9250215c8b94509a4a
Eoin Woods <github@copse.org.uk>
2018-07-03 22:26:55 +0000

    make load generator CPU count aware

--- bin/generate-load.sh
+++ bin/generate-load.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 #
-# generate-load.sh - use "openssl speed" to generate CPU load on the machine
-#                    runs on a single cpu
+# generate-load.sh - use "openssl speed" to generate significant CPU load on
+#                    on all of the machine's cores
 #
+
+cpu_count=$(grep -c ^processor /proc/cpuinfo)
 while true
 do
-   openssl speed > /dev/null 2>&1
+   openssl speed -multi $cpu_count > /dev/null 2>&1
 done
