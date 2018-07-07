e3ea75e28a6229c6416fb1a3c56cbd692a54a535
Eoin Woods <github@copse.org.uk>
2018-07-07 21:11:43 +0000

    Fixes for scripts

--- bin/package-results.sh
+++ bin/package-results.sh
@@ -7,12 +7,14 @@ then
    echo "USAGE: $0 scenarioname"
    exit 1
 fi
+scenario_name=$1
 
 date_string=$(date +%Y%m%d-%H%M%S)
-file_name="${1}-${date_string}.tar"
+file_name="${scenario_name}-${date_string}.tar"
 
 file_list="zipkin_db.sql telegraf.line.dmp \
            docker_network.json pidstat.out sar_stats.out"
 
+echo "========== Packaged results for $scenario_name into $file_name"
 tar cf ${file_name} ${file_list}
 
--- bin/run-scenarios.sh
+++ bin/run-scenarios.sh
@@ -4,9 +4,10 @@
 #                    export the data from the MySQL and InfluxDB databases, 
 #                    to capture traces and resource utilisation data sets.
 #
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOTDIR=${ROOTDIR:-$PWD}
 PATH=$PATH:$ROOTDIR/bin
-DEMO_SCENARIOS="single-cpu three-cpu single-cpu data-500mb-4times cpu-data-mix"
+DEMO_SCENARIOS="single-cpu"
 
 if [ $# -eq 0 ]
 then
@@ -15,39 +16,37 @@ else
   scenario_list=$*
 fi
 
-clear-databases.sh
-
-pidstat -C java -d -u -h -I 1 > pidstat.out 2>&1 &
-
-echo "========== Waiting 30 seconds to collect initial data"
-sleep 30
-
 echo "========== Running scenarios: $scenario_list"
 for s in $scenario_list
 do
+   $SCRIPTDIR/clear-databases.sh
+
+   pidstat -C java -d -u -h -I 1 > pidstat.out 2>&1 &
+
+   echo "========== Waiting 20 seconds to collect initial data"
+   sleep 20
+
    echo Running $s
    curl http://localhost:9999/invoke/$s
    echo Sleeping 5 seconds
    sleep 5
+
+   echo "========== Waiting another 15 seconds to collect data"
+   sleep 15
+
+   # Stop writing process level statistics
+   killall -HUP pidstat
+
+   # Collect sar(1m) stats for CPU, I/O and memory from 10 mins ago to now
+   # The explicit LC_TIME setting is to set 24 hour times
+   let start_time_secs=$(date +%s)-600
+   start_time="$(date -d @$start_time_secs +%H:%M:%S)"
+   sar_output_file=sar_stats.out
+   LC_TIME=C sar -P ALL -s $start_time > $sar_output_file   # CPU
+   LC_TIME=C sar -r     -s $start_time >> $sar_output_file  # Memory
+   LC_TIME=C sar -b     -s $start_time >> $sar_output_file  # I/O
+
+   $SCRIPTDIR/export-data.sh
+   $SCRIPTDIR/package-results.sh $s
 done
-
-echo "========== Waiting another 25 seconds to collect data"
-sleep 20
-
-# Stop writing process level statistics
-killall -HUP pidstat
-
-# Collect sar(1m) stats for CPU, I/O and memory from 10 mins ago to now
-# The explicit LC_TIME setting is to set 24 hour times
-let start_time_secs=$(date +%s)-600
-start_time="$(date -d @$start_time_secs +%H:%M:%S)"
-sar_output_file=sar_stats.out
-LC_TIME=C sar -P ALL -s $start_time > $sar_output_file   # CPU
-LC_TIME=C sar -r     -s $start_time >> $sar_output_file  # Memory
-LC_TIME=C sar -b     -s $start_time >> $sar_output_file  # I/O
- 
-export-data.sh
-
-
-
 
