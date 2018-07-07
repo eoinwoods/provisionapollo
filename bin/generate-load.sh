ddd25353ed767cc4d8cd2e99841658eac119c2d6
Eoin Woods <github@copse.org.uk>
2018-07-07 20:24:28 +0000

    Allow setting of utilisation level

--- bin/generate-load.sh
+++ bin/generate-load.sh
@@ -5,7 +5,18 @@
 #
 
 cpu_count=$(grep -c ^processor /proc/cpuinfo)
+parallel_degree=$cpu_count
+if [ ! -z "$1" ]
+then
+   if [ $1 -ge 1 -a $1 -le $cpu_count ]
+   then
+      parallel_degree=$1
+   else
+      echo "Invalid parallel degree of $1 -- exiting"
+      exit 1
+   fi
+fi
 while true
 do
-   openssl speed -multi $cpu_count > /dev/null 2>&1
+   openssl speed -multi $parallel_degree > /dev/null 2>&1
 done
